678dcde302dd543753a092a757d0d0a91bfdbb1b
James Johnson <jamesjohnson11@gmail.com>
2015-04-03 17:33:39 -0400

    added fixes

--- api.py
+++ api.py
@@ -6,27 +6,27 @@ import requests
 from instagram.client import InstagramAPI
 from models import Session, ScrapeRequest, User
 
-CODE = "fd9c0acc616f41c4801eed3fa7c7ca14"
+CODE = ""
 
 """
-curl \-F 'client_id=393749080807444a975e11c6060a58be' \
-    -F 'client_secret=95775738376b4cb58b2b69e30492cd91' \
+curl \-F 'client_id=' \
+    -F 'client_secret=' \
     -F 'grant_type=authorization_code' \
     -F 'redirect_uri=http://localhost/instagram' \
-    -F 'code=fd9c0acc616f41c4801eed3fa7c7ca14' \https://api.instagram.com/oauth/access_token
+    -F 'code=' \https://api.instagram.com/oauth/access_token
 """
 
 
 import hmac
 from hashlib import sha256
 
-ips = '72.69.141.254'
-secret = '6dc1787668c64c939929c17683d7cb74'
+ips = ''
+secret = ''
 
 signature = hmac.new(secret, ips, sha256).hexdigest()
 header = '|'.join([ips, signature])
 
-access_token = "6767180.3937490.c6dfa67eb594468aa4aa9fe6cd2539a0"
+access_token = ""
 api = InstagramAPI(access_token=access_token)
 count = 0
 while True:
